13289712c21f643ccadd98aa76d4b700bdf01959
mohit04gits <mohitgupta1834@gmail.com>
2025-03-28 12:11:10 +0530

    Update app.js

--- src/app.js
+++ src/app.js
@@ -17,7 +17,112 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(cors(
   {
-    origin:"http://localhost:5173",
+    origin:["http://localhost:5173",const express = require("express");
+const app = express();
+const cors = require("cors")
+const User = require("./model/user")
+const connectDB = require("./config/database");
+const cookieParser = require("cookie-parser");
+const authRouter = require("./routes/auth");
+const profileRouter = require("./routes/profile");
+const sendConnectionRequest = require("./routes/requests");
+const requestRouter = require("./routes/requests");
+const { userAuth } = require("./middlewares/auth");
+const userRouter = require("./routes/user");
+
+require("dotenv").config();
+// const jwt = require("jsonwebtoken");
+app.use(express.json());
+app.use(cookieParser());
+app.use(cors(
+  {
+    origin:["http://localhost:5173","https://devtinderfrontend.onrender.com"]
+    credentials:true,
+  }
+));
+
+
+
+
+
+
+app.use("/", authRouter);
+app.use("/", profileRouter);
+app.use("/", requestRouter);
+app.use("/",userRouter);
+
+
+//feed
+app.get("/feed",userAuth, async (req, res) => {
+  try {
+    const users = await User.find({});
+    res.send(users);
+  } catch (err) {
+    res.status(404).send("something went wrong");
+    console.log(err);
+  }
+});
+
+app.delete("/user", async (req, res) => {
+  const userId = req.body.userId;
+  try {
+    const users = await User.findByIdAndDelete(userId);
+    res.send("user deleted succesfully");
+  } catch (err) {
+    {
+      res.status(404).send("something went wrong");
+    }
+  }
+});
+
+
+
+//update
+app.patch("/user/:userId", async (req, res) => {
+  const userId = req.params?.userId;
+  const data = req.body;
+
+  try {
+    const Allowed_Updates = ["skills", "about", "age", "gender"];
+    const isUpdateAllowed = Object.keys(data).every((key) =>
+      Allowed_Updates.includes(key)
+    );
+
+    if (!isUpdateAllowed) {
+      throw new Error("Update not allowed for one or more fields");
+    }
+
+    const user = await User.findByIdAndUpdate(userId, data, {
+      //new: true, // Return the updated document
+      returnDocument: "after",
+      runValidators: true, // Ensure validation rules are applied
+    });
+
+    // if (!user) {
+    //   return res.status(404).send("User not found");
+    // }
+
+    // res.status(200).send({ message: "User updated successfully", user });
+    res.send(user);
+  } catch (err) {
+    res.status(400).send("Update failed: " + err.message);
+  }
+});
+
+//PROFILE
+
+connectDB()
+  .then(() => {
+    console.log("Database connected successfully");
+
+    app.listen(process.env.PORT || 3000, () => {
+      console.log("server started on server 3000");
+    });
+  })
+
+  .catch((err) => {
+    console.log("Database not connected");
+  });
     credentials:true,
   }
 ));
