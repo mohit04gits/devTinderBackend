553a1eaa1ea4a15b2eb74eda8eea2e0b9172eeb6
mohit04gits <mohitgupta1834@gmail.com>
2025-03-28 12:46:19 +0530

    Update app.js

--- src/app.js
+++ src/app.js
@@ -20,7 +20,7 @@ app.use(cookieParser());
 app.use(
   cors({
     origin: ["http://localhost:5173", "https://devtinderfrontend.onrender.com"], // Allow both local and deployed frontend
-    methods: ["GET", "POST", "PUT", "DELETE"], // Specify allowed methods
+    methods: ["GET", "POST", "PUT", "DELETE","PATCH"], // Specify allowed methods
     allowedHeaders: ["Content-Type", "Authorization"], // Allow necessary headers
     credentials: true, // Must be placed correctly (not inside 'origin' array)
   })
